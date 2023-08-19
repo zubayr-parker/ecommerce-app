957379814aacea876d3e2795277ee3caf27b5237
zubayr-parker <zubayr_parker@outlook.com>
2023-08-19 13:46:01 +1000

    recommended typo

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -83,7 +83,7 @@ const BookInfo = ({ books, addToCart, cart }) => {
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
-              <h2 className="book__selected--title--top">Reccomended Books</h2>
+              <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
             <div className="books">
               {books
